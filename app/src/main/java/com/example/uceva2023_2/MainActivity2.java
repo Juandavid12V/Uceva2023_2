3bff306453382487df957680ef605a4a8efb7249
Juan Vega <juan.vega01@uceva.edu.co>
2023-10-08 17:04:01 -0500

    Hola

--- app/src/main/java/com/example/uceva2023_2/MainActivity2.java
+++ app/src/main/java/com/example/uceva2023_2/MainActivity2.java
@@ -2,11 +2,13 @@ package com.example.uceva2023_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity  implements  View.OnClickListener{
 
     Button bnt2,bnt3;
 
@@ -17,5 +19,26 @@ public class MainActivity2 extends AppCompatActivity {
         bnt2 = findViewById(R.id.btnLogIn2);
         bnt3 = findViewById(R.id.btnLogIn3);
         Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+        bnt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "hola 1", Toast.LENGTH_LONG).show();
+            }
+        });
+        bnt3.setOnClickListener(this);
+    }
+
+
+    public void saludar(View d){
+        //Toast.makeText(this, "Hola 2", Toast.LENGTH_LONG).show();
+        Intent ir = new Intent(this, MainActivity3.class);
+        ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TOP | ir.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(ir);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Hola 3", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/uceva2023_2/MainActivity3.java
+++ app/src/main/java/com/example/uceva2023_2/MainActivity3.java
@@ -2,7 +2,10 @@ package com.example.uceva2023_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause",Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy",Toast.LENGTH_LONG).show();
+    }
+
+    public void saludar(View d){
+        //Toast.makeText(this, "Hola 2", Toast.LENGTH_LONG).show();
+        Intent ir = new Intent(this, MainActivity2.class);
+        startActivity(ir);
     }
 }
